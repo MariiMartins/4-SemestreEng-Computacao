23541910688a5587ca971b9369bf64f99b27a08a
Mariana Martins <42555083+MariiMartins@users.noreply.github.com>
2018-10-16 16:40:04 -0300

    Add files via upload

---
+++ Calculadora .py
@@ -0,0 +1,11 @@
+num1 = input ("Digite um numero ")
+num2 = input ("Digite outro numero ")
+
+soma = float (num1) + float (num2)
+sub = float (num1) - float (num2)
+multi = float (num1) * float (num2)
+div = float (num1) / float (num2)
+porc = float (num1) % float (num2)
+exp = float (num1) ** float (num2)
+
+print (soma , sub, multi, div, porc, exp)
---
+++ Conversão .py
@@ -0,0 +1,21 @@
+fare = input("Digite um valor em Fareheint ")
+celsius = (float (fare) - 32) / 1.8
+	
+print("O valor em Celsius é " + str (celsius))
+
+#
+celsius = input("Digite um valor em Celsius ")
+fare = (float (celsius) * 1.8) + 32
+
+print("O valor em Fareheint é " +str(fare))
+
+#
+kmh = input ("Digite um valor de Quilometragem ")
+ms = float  (kmh) / 3.6
+
+print ("O valor é " +str (ms) + "M/S")
+
+#
+met = input ("Digite um valor em Metros ")
+rkmh = float (met) * 3.6
+print ("O valor é "+str (rkmh) + "KM/H")
